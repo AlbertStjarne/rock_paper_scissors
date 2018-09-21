328fb11b2b4ec0b5e154e038bd608d7344874847
Albert Stjärne <albert.stjarne@gmail.com>
2018-09-21 09:56:04 +0200

    adds index file and start of connecting with the js

--- src/js/rock-paper-scissors.js
+++ src/js/rock-paper-scissors.js
@@ -1,5 +1,63 @@
-function RockPaperScissors()  {
-  this.check = (number) => {
-      return number
+// function RockPaperScissors()  {
+//   this.check = (number) => {
+//       return number
+//   }
+// }
+
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissor') {
+    return userInput;
+  } else {
+    console.log('Invalid input');
   }
 }
+
+const getComputerChoice = () => {
+  randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+    return 'rock';
+    case 1:
+    return 'paper';
+    case 2:
+    return 'scissor';
+  };
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'Game was a tie';
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'Computer wins';
+    } else {
+      return 'You win';
+    }
+  }
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissor') {
+      return 'Computer wins';
+    } else {
+      return 'You win';
+    }
+  }
+  if (userChoice === 'scissor') {
+    if (computerChoice === 'rock') {
+      return 'Computer wins';
+    } else {
+      return 'You win';
+    }
+  }
+}
+
+// const playGame = () => {
+//   const userChoice = getUserChoice('rock');
+//   const computerChoice = getComputerChoice();
+//   console.log(`You threw: ${userChoice}`);
+//   console.log(`The computer threw: ${computerChoice}`);
+//   console.log(determineWinner(userChoice, computerChoice));
+// }
+
+// playGame();
